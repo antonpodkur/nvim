17ce54799538bd08828488e33e92bcc20bf33a47
Anton Podkur <anton.podkur@gmail.com>
2023-09-15 20:49:58 +0300

    Added mappings for bufferline

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -1,3 +1,7 @@
 vim.opt.termguicolors = true
 require("bufferline").setup{}
 
+vim.keymap.set('n', '<Tab>', '<Cmd>BufferLineCycleNext<CR>', {})
+vim.keymap.set('n', '<S-Tab>', '<Cmd>BufferLineCyclePrev<CR>', {})
+vim.keymap.set('n', '<leader>bo', '<Cmd>BufferLinePick<CR>', {})
+vim.keymap.set('n', '<leader>bc', '<Cmd>BufferLinePickClose<CR>', {})
