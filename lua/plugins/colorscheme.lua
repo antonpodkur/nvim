727db7fe42429f75a502aab12c852673e661592b
Anton Podkur <anton.podkur@gmail.com>
2024-08-02 01:17:51 +0300

    Changed theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -16,19 +16,30 @@
 --   opts = function()
 --     return {
 --       transparent = false,
---       theme = "dragon",
+--       theme = "lotus",
 --       background = "",
 --     }
 --   end,
 -- }
 
+-- return {
+--   "ellisonleao/gruvbox.nvim",
+--   lazy = true,
+--   priority = 1000,
+--   opts = function()
+--     return {
+--       transparent = false,
+--     }
+--   end,
+-- }
+
 return {
-  "ellisonleao/gruvbox.nvim",
-  lazy = true,
-  priority = 1000,
-  opts = function()
-    return {
-      transparent = false,
-    }
+  "projekt0n/github-nvim-theme",
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
+  config = function()
+    require("github-theme").setup({
+      -- ...
+    })
   end,
 }
